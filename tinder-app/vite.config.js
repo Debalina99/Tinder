c34862bbd824fb7f5bce307d3f055d37ca78b563
Debalina99 <debalinalaha13630@gmail.com>
2024-10-17 00:59:11 +0530

    changes in vite config file

--- tinder-app/vite.config.js
+++ tinder-app/vite.config.js
@@ -4,8 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  root: './',
   build: {
-    outDir: 'dist',
+    outDir: 'dist', // This should match the output directory in Vercel settings
   },
 });
