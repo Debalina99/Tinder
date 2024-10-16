9a65fcff6b725b8d0389958afadb211e7c0e339a
Debalina99 <debalinalaha13630@gmail.com>
2024-10-16 22:38:42 +0530

    few changes in home page

--- tinder-app/src/components/Home.js
+++ tinder-app/src/components/Home.js
@@ -10,7 +10,7 @@ const Home = () => {
             }}>
             <Navbar />
 
-            <div className="relative flex flex-col items-center justify-center h-full text-white pt-16">
+            <div className="relative flex flex-col items-center justify-center h-full text-white">
                 <h1 className="text-8xl font-bold text-gray-800 ">Start Something epic.</h1>
                 <Link to="/signup" className="bg-red-500 text-white px-5 py-2 my-2 rounded-full text-lg font-bold transition duration-300 hover:bg-pink-500" style={{ borderRadius: "20px", }}>
                     Create account
