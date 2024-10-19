5d32361476784e96b68c9cdee9e4877866b9921c
Debalina99 <debalinalaha13630@gmail.com>
2024-10-19 11:18:57 +0530

    changes position of logo

--- tinder-app/src/components/Navbar.jsx
+++ tinder-app/src/components/Navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = () => {
         <div>
             <nav className=" p-4 flex justify-between items-center">
                 <div className="container mx-auto flex justify-between items-center">
-                    <div className="flex items-center ml-4">
+                    <div className="flex items-center ml-8">
                         <img src="https://cdn.iconscout.com/icon/free/png-512/free-tinder-logo-icon-download-in-svg-png-gif-file-formats--social-media-communication-network-pack-logos-icons-3201567.png?f=webp&w=256https://cdn.iconscout.com/icon/free/png-512/free-tinder-logo-icon-download-in-svg-png-gif-file-formats--technology-social-media-company-logos-pack-icons-6297329.png?f=webp&w=256https://cdn.iconscout.com/icon/free/png-512/free-tinder-logo-icon-download-in-svg-png-gif-file-formats--technology-social-media-company-logos-pack-icons-6297329.png?f=webp&w=256" className="h-12 w-12 mr-2" alt="tinder logo" />
                         <Link to="/" className="text-white text-4xl font-bold">tinder</Link>
                     </div>
