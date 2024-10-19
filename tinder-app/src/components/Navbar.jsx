7a3fb42c88619c6181e0b3a85b04227aff55a3d5
Debalina99 <debalinalaha13630@gmail.com>
2024-10-19 11:17:21 +0530

    changes position of logo

--- tinder-app/src/components/Navbar.jsx
+++ tinder-app/src/components/Navbar.jsx
@@ -6,9 +6,9 @@ const Navbar = () => {
         <div>
             <nav className=" p-4 flex justify-between items-center">
                 <div className="container mx-auto flex justify-between items-center">
-                    <div className="flex items-center">
-                        <img src="https://cdn.iconscout.com/icon/free/png-512/free-tinder-logo-icon-download-in-svg-png-gif-file-formats--social-media-communication-network-pack-logos-icons-3201567.png?f=webp&w=256https://cdn.iconscout.com/icon/free/png-512/free-tinder-logo-icon-download-in-svg-png-gif-file-formats--technology-social-media-company-logos-pack-icons-6297329.png?f=webp&w=256https://cdn.iconscout.com/icon/free/png-512/free-tinder-logo-icon-download-in-svg-png-gif-file-formats--technology-social-media-company-logos-pack-icons-6297329.png?f=webp&w=256" className="h-10 w-10 mr-2" alt="tinder logo" />
-                        <Link to="/" className="text-white text-2xl font-bold">tinder</Link>
+                    <div className="flex items-center ml-4">
+                        <img src="https://cdn.iconscout.com/icon/free/png-512/free-tinder-logo-icon-download-in-svg-png-gif-file-formats--social-media-communication-network-pack-logos-icons-3201567.png?f=webp&w=256https://cdn.iconscout.com/icon/free/png-512/free-tinder-logo-icon-download-in-svg-png-gif-file-formats--technology-social-media-company-logos-pack-icons-6297329.png?f=webp&w=256https://cdn.iconscout.com/icon/free/png-512/free-tinder-logo-icon-download-in-svg-png-gif-file-formats--technology-social-media-company-logos-pack-icons-6297329.png?f=webp&w=256" className="h-12 w-12 mr-2" alt="tinder logo" />
+                        <Link to="/" className="text-white text-4xl font-bold">tinder</Link>
                     </div>
                     <div>
                         <Link to="/products" className="text-white px-3 font-bold">Products</Link>
