fb4833b52900be85d7f03ae9347c31de88c51fbd
Debalina99 <debalinalaha13630@gmail.com>
2024-10-22 00:52:48 +0530

    Added styles in components

--- tinder-app/src/components/Home.jsx
+++ tinder-app/src/components/Home.jsx
@@ -10,7 +10,7 @@ const Home = () => {
             <Navbar />
 
             <div className="container relative flex flex-col items-center justify-center h-full text-white">
-                <h1 className="text-4xl md:text-6xl lg:text-8xl font-bold text-gray-200 text-center">
+                <h1 className="text-4xl md:text-6xl lg:text-8xl font-bold text-gray-500 text-center">
                     Start Something Epic.
                 </h1>
                 <Link to="/signup" className="bg-red-500 text-white px-4 py-2 my-4 rounded-full text-lg font-bold transition duration-300 hover:bg-pink-500" style={{ borderRadius: "20px" }}>
