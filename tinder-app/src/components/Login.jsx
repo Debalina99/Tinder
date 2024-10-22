f459d04d3dcd73493171e69dad5111d3c418d2fd
Debalina99 <debalinalaha13630@gmail.com>
2024-10-22 11:27:16 +0530

    Made changes in login component

--- tinder-app/src/components/Login.jsx
+++ tinder-app/src/components/Login.jsx
@@ -15,31 +15,22 @@ const Login = () => {
 
         try {
             const userData = { email, password };
-            await login(userData);
+            const response = await login(userData);
+            // console.log("Login successful, response: ", response);
             navigate('/feed'); 
         } catch (error) {
+            console.log("Error during login: ", error);
             setError(error.response?.data?.message || 'Invalid email or password');
         }
     }
     return (
         <div className="min-h-screen bg-gradient-to-r from-red-400 to-orange-300 flex items-center justify-center px-3">
-            <form 
-                onSubmit={submitHandler} 
-                className="bg-white p-8 rounded shadow-md w-full max-w-sm"
-            >
+            <form onSubmit={submitHandler} className="bg-white p-8 rounded shadow-md w-full max-w-sm">
                 <h2 className="text-3xl mb-4 text-center md:text-4xl">Login</h2>
                 {error && <p className="text-red-500 mb-4">{error}</p>}
-                <input 
-                    type="email" name="email" placeholder="Enter email" value={email} onChange={(e) => setEmail(e.target.value)} 
-                    className='block w-full p-3 border rounded mb-4 text-lg'
-                />
-                <input 
-                    type="password" name="password" placeholder="Enter password" value={password} onChange={(e) => setPassword(e.target.value)} 
-                    className='block w-full p-3 border rounded mb-4 text-lg'
-                />
-                <button 
-                    type="submit" className="w-full p-3 my-4 text-white rounded-full text-lg font-bold transition duration-300 bg-red-400 hover:bg-red-500">Login
-                </button>
+                <input type="email" name="email" placeholder="Enter email" value={email} onChange={(e) => setEmail(e.target.value)} className='block w-full p-3 border rounded mb-4 text-lg'/>
+                <input type="password" name="password" placeholder="Enter password" value={password} onChange={(e) => setPassword(e.target.value)} className='block w-full p-3 border rounded mb-4 text-lg'/>
+                <button type="submit" className="w-full p-3 my-4 text-white rounded-full text-lg font-bold transition duration-300 bg-red-400 hover:bg-red-500">Login</button>
                 <div className="flex justify-center">
                     <p className="">Don't have an account? </p>
                     <Link to="/signup" className="text-red-500 mx-1 font-semibold">Create account</Link>
